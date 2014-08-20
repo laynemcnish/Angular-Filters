4e76fb41f3d3788485ccc20fb95dbc7eab7116f4
Peter Grunde <peter.a.grunde@gmail.com>
2014-08-19 23:09:16 -0600

    added titleCase filter to Reverse Letter

--- Filter Examples/Reverse Letter/js/main.js
+++ Filter Examples/Reverse Letter/js/main.js
@@ -1,21 +1,31 @@
-var myApp = angular.module('myApp',[]);
-myApp.factory('Data', function(){
-  return {message:"I'm data from a service"}
+var myApp = angular.module('myApp', []);
+
+myApp.factory('Data', function () {
+  return {message: "I'm data from a service"}
 });
 
-myApp.filter('reverse', function(Data){
-  return function(text) {
-    return text.split("").reverse().join("") + " " + Data.message;
-  }
-});
 
 function FirstCtrl($scope, Data) {
   $scope.data = Data;
 }
 function SecondCtrl($scope, Data) {
   $scope.data = Data;
-
-  $scope.reversedMessage = function(message){
-    return message.split("").reverse().join("");
-  }
 }
+
+
+myApp.filter('reverse', function (Data) {
+  return function (text) {
+    return text.split("").reverse().join("") + " " + Data.message;
+  }
+});
+
+myApp.filter('titleCase', function () {
+  var titleCaseFilter = function(input) {
+    var words = input.split(' ');
+    for (var i=0; i < words.length; i++) {
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(' ');
+  };
+  return titleCaseFilter;
+});
